0bbdd65a513776d687683c1749cf78856a1fbd19
FawadAhmad001 <FawadAhmad001@github.com>
2025-05-09 01:33:48 +0500

    issue Fix

--- shared/constants/apiConstants/index.ts
+++ shared/constants/apiConstants/index.ts
@@ -1,13 +1,14 @@
 // const BASE_URL =  'http://localhost:8000';
 
-const BASE_URL =  'https://fortifi.world';
+// const BASE_URL =  'https://fortifi.world';
+const BASE_URL = 'https://api.fortifi.world'
 
 const COINGECKO_PUBLIC = 'https://api.coingecko.com/api/v3';
 const COINGECKO = 'https://pro-api.coingecko.com/api/v3';
 
 const END_POINT = {
     LOGIN: '/api/admin/Login', 
-    GET_CONTACTS:'/api/contacts',
+    GET_CONTACTS:'/contact/getContacts',
     UPDATE_KYC:'/api/kyc/update/{id}',
     GET_KYC:'/api/kyc',
     GET_SINGLE_KYC:'/api/kyc/{id}',
